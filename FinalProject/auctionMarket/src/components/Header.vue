8684d36dbf1cc97332720a160f1bf5f2a69396ec
JongchanJeon <tristan6349@naver.com>
2022-11-14 15:04:46 +0900

    add vue

---
+++ FinalProject/auctionMarket/src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+<header>
+  <div class="navbar navbar-default">
+    <div class="navbar-center">
+      <h1><router-link :to="{name: 'iMain'}">{{ sitename }}</router-link></h1>
+    </div>
+    <div class="nav navbar-nav navbar-right cart">
+      <!-- <button type="button" class="btn btn-default btn-lg" v-on:click="showCheckout">
+        <span class="glyphicon glyphicon-shopping-cart">{{cartItemCount}}</span> 체크아웃
+      </button> -->
+      <router-link
+          active-class="active"
+          tag="button" class="btn btn-default btn-lg" :to="{name: 'Form'}">
+        <span class="glyphicon glyphicon-shopping-cart">{{cartItemCount}}</span> My Page
+
+      </router-link>
+      <router-link
+          active-class="active"
+          tag="button" class="btn btn-default btn-lg" :to="{name: 'Form'}">
+
+        <span class="glyphicon glyphicon-shopping-cart">{{cartItemCount}}</span> Sign Out
+      </router-link>
+    </div>
+  </div>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'my-header',
+  data() {
+    return {
+      sitename: "Auction Market with Vus.js"
+    }
+  },
+  props: ['cartItemCount'],
+  methods: {
+    // showCheckout() {
+    //   this.$router.push({name: 'Form'});
+    // }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.router-link-exact-active {
+  /* color: blue; */
+  color: black;
+}
+</style>
+
